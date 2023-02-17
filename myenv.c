a7ac24d84abd2bbb84ccfb95a6ca34dcf146cfba
David <100460314@alumnos.uc3m.es>
2023-02-17 19:54:13 +0100

    Iniciating myenv.c

--- myenv.c
+++ myenv.c
@@ -2,6 +2,8 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <fcntl.h>
+
 
 
 
@@ -14,6 +16,27 @@ int main(int argc, char *argv[])
     	printf("Too few arguments\n");
     	return -1;
     }
+
+    int fd = open(argv[1], O_RDONLY);
+    if (fd == -1) {
+        printf("Error opening file %s\n", argv[1]);
+        return -1;
+    }
+
+    int op= open(argv[2],O_CREAT | O_RDWR);
+
+    if (op == -1) {
+        printf("Error opening file %s\n", argv[1]);
+        return -1;
+    }
+
+    char searching=argv[1];
+    char buffer;
+    int n;
+    while ((n = read(fd, buffer, 1)) > 0) {
+        ;
+    }
+
 
     return 0;
 }
