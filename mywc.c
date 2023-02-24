b652095cc1d42cfecb44b38fd6e4622c9c080213
100472175 <100472175@alumnos.uc3m.es>
2023-02-24 12:16:23 +0100

    Fixed ls and wc for every case. Remaining to do menv.c, as it does not pass the tests.

--- mywc.c
+++ mywc.c
@@ -54,7 +54,6 @@ int main(int argc, char *argv[]) {
     // fd is the name given to the file inside the code, to reference the open file
     // CLOSE the input file.
     close(fd);
-    lines++;
 
     // Check if there was an error reading the file, and print an error message if there was.
     if (n == -1) {
