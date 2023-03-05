32c84f067de3dc9412eaa1dacc69fc956e857fab
100472175 <100472175@alumnos.uc3m.es>
2023-03-05 19:06:57 +0100

    Added the new Zip, with the myenv.c fixed, as it searched for all variables such as ATH for PATH. Now, it doesn't

--- old_myenv.c
+++ old_myenv.c
@@ -11,37 +11,23 @@ int main(int argc, char *argv[]) {
         printf("Too few arguments. \nUsage: %s <input file> <string> <output file>\n", argv[0]);
         return -1;
     }
-    // Open the input file for reading, if the parameter is an argument:
-    //int fd = open(argv[1], O_RDONLY);
-    //if (fd < 0) {
-        //printf("Error opening file for reading %s\n", argv[1]);
-        //return -1;
-    //}
-    //As it is not:
-    int fd = open("env.txt", O_RDONLY);
+    // Open the input file for reading.
+    int fd = open(argv[1], O_RDONLY);
     if (fd < 0) {
-        printf("Error opening file for reading env.txt\n");
+        printf("Error opening file for reading %s\n", argv[1]);
         return -1;
     }
-
-
     // Open the output file, and if it already exists, it only opens it (O_CREAT)
-    int fdo = open(argv[2], O_CREAT|O_WRONLY|O_TRUNC, 0664);
+    int fdo = open(argv[3], O_CREAT|O_WRONLY|O_TRUNC, 0664);
     if (fdo < 0) {
-        printf("Error opening file to write %s\n", argv[2]);
+        printf("Error opening file to write %s\n", argv[3]);
         return -1;
     }
 
-    char character; // Where the character from the file is stored temporarily
+    int character; // Where the character from the file is stored temporarily
     char *linea; // Each of the lines individually processed
     char *pointer;
     int line_length = 0;
-
-    char *needle;
-    needle = malloc(strlen(argv[1])+2);
-    strncpy(needle, argv[1], strlen(argv[1]));
-    strncat(needle, "=",1);
-
     ssize_t n;
     linea = malloc(2);
     //linea2 = malloc(2);
@@ -56,33 +42,36 @@ int main(int argc, char *argv[]) {
         return(-1);
     }
     while (n > 0) {
-        while ((character != '\n') && (n > 0)){
-            //printf("character => #%s#\n", &character);
-            //while ((character != '\n') && (n > 0)) {
+        while ((character != '\n') && (n > 0)) {
             line_length++;
-            pointer = realloc(linea, strlen(linea)+2);
+            pointer = realloc(linea, strlen(linea)+1);
             linea = pointer;
+            // linea = realloc(linea, 1+line_length);
+            // printf("%p. In the line %d\n", linea, __LINE__);
             strncat(linea, (const char *) &character, 1);
+            //printf("%s\n", linea);
+            // Prepared for the next character
             n = read(fd, &character, 1);
         }
-        //printf("Antes de comparar en la linea %i\n", __LINE__);
-        if ((strstr(linea, needle) != NULL) && (linea[0] == needle[0])){
+        //printf("%s, %d\n", linea, __LINE__);
 
-            //printf("Match, con linea -> #%s#\n", linea);
+        //Add the \n character to the end of the line:
+        //linea = realloc(linea, 1);
+        //strcat(linea, "\n");
+
+        //printf("Pre-Trace, %d\n", __LINE__);
+        // Compare with argv[1]
+        //printf("Line: %s -- Argument: %s -- %d", linea, argv[2], __LINE__);
+        if (strstr(linea, argv[2]) != NULL) {
+            //printf("Trace \nLine being #%s#\n", linea);
+            // write(fdo, linea, line_length);
             if (write(fdo, linea, line_length) < line_length) {
                 perror("Writing File");
                 close(fd);
                 close(fdo);
                 exit(-1);
             }
-
-            //Adds a new line, as the exercise was undestood as all the ocurrences of the pattern
             write(fdo, "\n", 1);
-            // This return helps to speed up the process as if an occurrence is found, it is written,
-            // and it will exit immediately
-            close(fd);
-            close(fdo);
-            exit(0);
         }
         free(linea);
         linea = malloc(2);
@@ -90,6 +79,7 @@ int main(int argc, char *argv[]) {
         line_length = 0;
         n = read(fd, &character, 1);
     }
+    // printf("Traza_final\n");
     close(fd);
     close(fdo);
     return 0;
